c9bbd84ba33cabfd8b86f1349e877585c6ad27dc
Taylan Karaman <taylank@incolink.org.au>
2018-08-27 11:02:17 +1000

    Add heroes route. Add dashboard component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';  // NgModel lives here
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+// Modules
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { FormsModule }      from '@angular/forms';  // NgModel lives here
+import { AppRoutingModule } from './/app-routing.module';
+
+// Components
+import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { MessagesComponent }    from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
+  imports: [  
+    // This list of modules are those that export components, directives, or pipes that the component templates in this module reference. 
+    // A component template can reference another component, directive, or pipe when the referenced class is declared in this module or 
+    // the class was imported from another module.
+    BrowserModule, 
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]  // bootstrap—the root component that Angular creates and inserts into the index.html host web page.
 })
 export class AppModule { }
